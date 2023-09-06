f4db82b66a258982123220292e8160697e83036f
SamuelRocha91 <samuel_sr@hotmail.com.br>
2023-09-06 20:34:00 -0300

    requisito 01

--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -1,6 +1,17 @@
 from src.models.ingredient import Ingredient  # noqa: F401, E261, E501
 
-
 # Req 1
+
+
 def test_ingredient():
-    pass
+    queijo = Ingredient('queijo mussarela')
+    other_mussarela = Ingredient('queijo mussarela')
+    assert queijo.name == 'queijo mussarela'
+    assert queijo.__repr__() == "Ingredient('queijo mussarela')"
+    assert queijo.__eq__('queijo provolone') is False
+    assert queijo.__eq__(other_mussarela) is True
+    bacon = Ingredient('bacon')
+    assert bacon.__hash__() != queijo.__hash__()
+    assert queijo.__hash__() == other_mussarela.__hash__()
+    assert queijo.restrictions == other_mussarela.restrictions
+    assert queijo.restrictions != bacon.restrictions
